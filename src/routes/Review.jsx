1618969f5d30720311bbcf9797cdc9f3d384d9d3
Pritam-Mondal18 <pritammondal18012003@gmail.com>
2025-07-27 01:31:40 +0530

    remove unused code

--- src/routes/Review.jsx
+++ src/routes/Review.jsx
@@ -1,32 +1,3 @@
-// import React, { forwardRef, useEffect } from "react";
-// import "./Review.css";
-
-// function Review({ reviewRef }) {
-//   useEffect(() => {
-//     // Dynamically load the Elfsight script
-//     const script = document.createElement("script");
-//     script.src = "https://static.elfsight.com/platform/platform.js";
-//     script.async = true;
-//     document.body.appendChild(script);
-
-//     // Cleanup script when the component unmounts
-//     return () => {
-//       document.body.removeChild(script);
-//     };
-//   }, []); // Runs only once on component mount
-//   return (
-//     <>
-//       <div ref={reviewRef} className="review-container">
-//         <div
-//           className="elfsight-app-9806db2a-012d-4423-9503-12f04faf6b61"
-//           data-elfsight-app-lazy
-//         ></div>
-//       </div>
-//     </>
-//   );
-// }
-// export default Review;
-
 import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import "./Review.css";
